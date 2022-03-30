9f3be1494ad44f516cbdd2faf8efb0f9a925db45
woolim <woolim@getluko.com>
2022-03-30 21:09:57 +0200

    nitro lambda

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@ import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  bridge: {
-    nitro: false,
+  nitro: {
+    preset: "lambda",
   },
   css: ["~/assets/index.scss"],
 });
