53cffdf0bc993bfc6b9aa1ab02c424d2fac78815
woolim <woolim@getluko.com>
2022-04-11 10:22:45 +0200

    snap scrolling

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="$style.container" :key="windowWidth">
-    <div v-for="(title, index) in Object.keys(growthData)" :key="index">
+    <div v-for="(title, index) in Object.keys(growthData)" :key="index" :class="$style.item">
       <RadarChart :title="title" :color="colors[index]" :class="$style.chart" />
     </div>
   </div>
@@ -19,18 +19,34 @@ const colors = ["blue", "red", "green", "yellow"];
 </script>
 
 <style lang="scss" module>
+$navbar-height: 64px;
+$result-bar-height: 50px;
+
 .container {
-  margin: 120px 0 60px;
-  min-height: 100vh;
+  margin: $navbar-height 0 60px;
+  height: calc(100vh - $navbar-height);
   display: grid;
   grid-template-columns: 1fr 1fr;
   justify-items: center;
   align-items: center;
   row-gap: 100px;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
 
   @media screen and (max-width: 1200px) {
     grid-template-columns: 1fr;
-    margin: 120px 0 120px;
+  }
+  @include below(xsmall) {
+    height: calc(100vh - $navbar-height - $result-bar-height);
+  }
+}
+.item {
+  display: flex;
+  align-items: center;
+  scroll-snap-align: start;
+  height: calc(100vh - $navbar-height);
+  @include below(xsmall) {
+    height: calc(100vh - $navbar-height - $result-bar-height);
   }
 }
 </style>
