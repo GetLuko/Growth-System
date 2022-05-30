5fcacf9c0f0023cd832df5059b1fc201aeb9e0f4
woolim <woolim@getluko.com>
2022-05-30 02:16:43 +0200

    Save map data in localStorage

--- src/states/storeGrowthData/storeGrowthData.ts
+++ src/states/storeGrowthData/storeGrowthData.ts
@@ -1,38 +1,54 @@
 import { ref } from "vue";
 import { IGrowthData, ILevelPoint, ILevelTitle } from "./types";
+import { useStorage } from "@vueuse/core";
 
-const growthData = ref<IGrowthData>({
-  Engineering: {
-    Frontend: 0,
-    Hardware: 0,
-    Mobile: 0,
-    DataEngineering: 0,
-    Ops: 0,
-    Backend: 0,
-    Firmware: 0,
-    MachineL: 0,
+const growthData = useStorage<IGrowthData>(
+  "growth-data",
+  {
+    Engineering: {
+      Frontend: 0,
+      Hardware: 0,
+      Mobile: 0,
+      DataEngineering: 0,
+      Ops: 0,
+      Backend: 0,
+      Firmware: 0,
+      MachineL: 0,
+    },
+    Execution: {
+      Delivery: 0,
+      Optimization: 0,
+      Communication: 0,
+      Quality: 0,
+    },
+    Supporting: {
+      GrowingTogether: 0,
+      Accomplishment: 0,
+      PeopleMinded: 0,
+      OrgDev: 0,
+    },
+    Strengthening: {
+      Mentorship: 0,
+      Recruitment: 0,
+      Evangelism: 0,
+      Community: 0,
+    },
   },
-  Execution: {
-    Delivery: 0,
-    Optimization: 0,
-    Communication: 0,
-    Quality: 0,
+  undefined,
+  {
+    serializer: {
+      read: (v: any) => JSON.parse(atob(v)),
+      write: (v: any) => btoa(JSON.stringify(v)),
+    },
   },
-  Supporting: {
-    GrowingTogether: 0,
-    Accomplishment: 0,
-    PeopleMinded: 0,
-    OrgDev: 0,
-  },
-  Strengthening: {
-    Mentorship: 0,
-    Recruitment: 0,
-    Evangelism: 0,
-    Community: 0,
+);
+
+const otherGrowthData = useStorage<IGrowthData | null>("other-growth-data", null, undefined, {
+  serializer: {
+    read: (v: any) => (v ? JSON.parse(atob(v)) : null),
+    write: (v: any) => btoa(JSON.stringify(v)),
   },
 });
-
-const otherGrowthData = ref<IGrowthData | null>(null);
 
 const pointToLevel: Array<ILevelPoint> = [
   { point: 0, level: "3.1" },
